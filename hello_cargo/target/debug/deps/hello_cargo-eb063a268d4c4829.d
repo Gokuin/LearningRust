2d242a46909c5d59e58252f97ff521c7b4229f64
Gokuin <twg12@pitt.edu>
2022-09-21 19:01:00 -0400

    finished chapter 1 learned how to use cargo to help with project creation and file mangement

---
+++ hello_cargo/target/debug/deps/hello_cargo-eb063a268d4c4829.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Documents\LearningRust\hello_cargo\target\debug\deps\hello_cargo-eb063a268d4c4829.rmeta: src\main.rs
+
+C:\Users\Admin\Documents\LearningRust\hello_cargo\target\debug\deps\hello_cargo-eb063a268d4c4829.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Admin\Documents\LearningRust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Admin\Documents\LearningRust\hello_cargo\src\main.rs
