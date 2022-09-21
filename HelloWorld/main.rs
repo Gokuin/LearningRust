7eb0dfeb301d7cd282c6954cd16326776a7b6200
Gokuin <twg12@pitt.edu>
2022-09-21 18:43:10 -0400

    Made first rust program, where job?

---
+++ HelloWorld/main.rs
@@ -0,0 +1,6 @@
+//Program written by Taittinger Gabelhart
+//Written on 9/21/2022 Using the book rust programing language rust 2018
+fn main() {
+    //main function is always the first funciton to be ran in a rust file
+    println!("Hello World!"); //this is actually calling a rust macro not a function hence the !
+}
