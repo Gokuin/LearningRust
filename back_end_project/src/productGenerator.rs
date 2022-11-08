90fb31d787cde07de59289a902b314bfd33c5f7f
Gokuin <twg12@pitt.edu>
2022-11-08 11:33:19 -0500

    more code added to PSG

--- back_end_project/src/productGenerator.rs
+++ back_end_project/src/productGenerator.rs
@@ -4,10 +4,15 @@ fn main(){
     for x in 0..10{
         //gen_Prod returns a tuple(id, prod_name, price, quantity_left, prod_manfact)
         let mut new_Prod = gen_Prod();
+        println!("INSERT INTO public.products(/n
+            prodid, prod_name, price, quantity_left, prod_manufactor)");
+        println!("  VALUES({}, {}, {}, {}, {});", (x + 1), new_Prod.0, new_Prod.1, new_Prod.2, new_Prod.3);
     }
     println!("-----End of Generation-----");
 }
 
+//function randomly picks a prod_name, price, quantity_left, and prod_Manufact and returns it
+//as a tuple.
 fn gen_Prod(){
 
 }
