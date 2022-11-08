fbec21507acfdc96fd5d674963a49486a3e05c39
Gokuin <twg12@pitt.edu>
2022-11-08 14:50:55 -0500

    added fixes to sql file

--- back_end_project/SQL/ProductTable_TestData_Insert_Query.sql
+++ back_end_project/SQL/ProductTable_TestData_Insert_Query.sql
@@ -1,21 +1,30 @@
 --SQL pre generated using ProductGenerator written in rust by Taittinger Gabelhart
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(1 , Xbox Series X , 72.99 , 0 , MSI);
+  VALUES(1 , 'Xbox Series X' , 72.99 , 0 , 'MSI');
+  
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(2 , PS5 , 72.99 , 25 , Bestbuy);
+  VALUES(2 , 'PS5' , 72.99 , 25 , 'Bestbuy');
+  
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(3 , Viking Hat , 899.99 , 1 , Sony);
+  VALUES(3 , 'Viking Hat' , 899.99 , 1 , 'Sony');
+  
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(4 , Xbox Series X , 499.99 , 13 , Sony);
+  VALUES(4 , 'Xbox Series X' , 499.99 , 13 , 'Sony');
+  
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(5 , MSI gaming monitor , 9.99 , 0 , FromSoftware);
+  VALUES(5 , 'MSI gaming monitor' , 9.99 , 0 , 'FromSoftware');
+  
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(6 , Mario sticker , 9.99 , 0 , Nintendo);
+  VALUES(6 , 'Mario sticker' , 9.99 , 0 , 'Nintendo');
+  
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(7 , MSI gaming monitor , 0.99 , 5 , MSI);
+  VALUES(7 , 'MSI gaming monitor' , 0.99 , 5 , 'MSI');
+  
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(8 , Xbox Series X , 899.99 , 50 , Crate and Barrel);
+  VALUES(8 , 'Xbox Series X' , 899.99 , 50 , 'Crate and Barrel');
+  
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(9 , Mario sticker , 72.99 , 25 , Nintendo);
+  VALUES(9 , 'Mario sticker' , 72.99 , 25 , 'Nintendo');
+  
 INSERT INTO public.products(prodid, prod_name, price, quantity_left, prod_manufactor)
-  VALUES(10 , Mario sticker , 1234.99 , 25 , MSI);
+  VALUES(10 , 'Mario sticker' , 1234.99 , 25 , 'MSI');
